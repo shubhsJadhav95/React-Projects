f11ef7cd9eef5f459c0d863600dd7de9c85eaf24
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2025-12-15 16:42:58 +0530

    update header

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrderPage } from "./pages/OrderPage";  
+import { TrackingPage } from "./pages/TrackingPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrderPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
