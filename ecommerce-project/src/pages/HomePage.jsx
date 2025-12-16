a2fa652a33443f0ac6c89bd923268a78ce3e91df
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2025-12-16 10:15:31 +0530

    fetch data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,12 @@ import './HomePage.css';
 import { products } from './products.js';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+  .then((response) => 
+
+    { response.json().then((data) => { console.log(data); });
+})
+  .catch((error) => { console.error('Error fetching products:', error); });
   return (
     <><Header />
       <body>
